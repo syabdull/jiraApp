9571b0433beb595831b4a3e58e050ec40269484c
syabdull <syab5050@gmail.com>
2022-09-25 11:46:39 +0530

    ticket functionality added

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let modalCont=document.querySelector(".modal-cont");
 let taskAreaCont=document.querySelector(".textarea-cont");
 let mainCont=document.querySelector(".main-cont");
 let allPriorityColors=document.querySelectorAll(".priority-color");
-let  modalPriorityColor='black';
+let colors=['lightpink','bule','green','black'];
+let  modalPriorityColor =colors[colors.length-1];
 let removeBtn=document.querySelector(".remove-btn");
 let addModal=true;
 let removeFlag=false;
+
  
 
 addBtn.addEventListener("click",function(){
@@ -76,8 +78,56 @@ modalCont.addEventListener("keydown",function(e){
             ticketCont.setAttribute('class','ticket-cont');
             ticketCont.innerHTML=`<div class="ticket-color ${ticketColor}"></div>
                                 <div class="ticket-id">#abd</div>
-                                <div class="task-area">${task}</div>`
+                                <div class="task-area">${task}</div>
+                                <div class="lock-unlock"><i class="fa fa-lock"></i></div>`
             mainCont.appendChild(ticketCont);
+
+
+            // toggle unlock lock handle
+             let lockUnlockBtn=ticketCont.querySelector(".lock-unlock i");
+              let ticketTaskArea=ticketCont.querySelector(".task-area");
+             lockUnlockBtn.addEventListener("click",function(){
+                 if(lockUnlockBtn.classList.contains("fa-lock")){
+                    lockUnlockBtn.classList.remove("fa-lock");
+                    lockUnlockBtn.classList.add("fa-unlock");
+                    ticketTaskArea.setAttribute("contenteditable","true");
+                 }else{
+                    lockUnlockBtn.classList.remove("fa-unlock");
+                    lockUnlockBtn.classList.add("fa-lock");
+                    ticketTaskArea.setAttribute("contenteditable","false");
+                 }
+
+             })
+
+
+            ticketCont.addEventListener("click",function(){
+                if(removeFlag)
+
+                ticketCont.remove();
+
+
+            })
+
+          let  ticketColorBand=  ticketCont.querySelector(".ticket-color");
+          ticketColorBand.addEventListener("click",function(){
+            let currentTicketColor= ticketColorBand.classList[1];
+            let currentTicketColorIdx=-1;
+
+            for(let i=0;i<colors.length;i++){
+                if(currentTicketColor==colors[i]){
+                    currentTicketColorIdx=i;
+                       break;
+                }
+            }
+            let nextColorIdx=(currentTicketColorIdx+1)%colors.length;
+            let nextColor=colors[nextColorIdx];
+            ticketColorBand.classList.remove(currentTicketColor);
+            ticketColorBand.classList.add(nextColor);
+
+
+
+          })
+             
 
 
 }
