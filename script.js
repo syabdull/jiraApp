dd91ee1e0fc2913b3ef6bd1ac5b4b5d643371785
syabdull <syab5050@gmail.com>
2022-09-25 09:54:00 +0530

    functionality added

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ let addBtn=document.querySelector(".add-btn");
 let modalCont=document.querySelector(".modal-cont");
 let taskAreaCont=document.querySelector(".textarea-cont");
 let mainCont=document.querySelector(".main-cont");
+let allPriorityColors=document.querySelectorAll(".priority-color");
+let  modalPriorityColor='black';
+let removeBtn=document.querySelector(".remove-btn");
 let addModal=true;
+let removeFlag=false;
  
 
 addBtn.addEventListener("click",function(){
@@ -13,14 +17,29 @@ addBtn.addEventListener("click",function(){
     }
           addModal=!addModal;
 
-});
+})
+
+   for(let i=0;i<allPriorityColors.length;i++){
+     let priorityDivOneColor=allPriorityColors[i];
+     priorityDivOneColor.addEventListener("click",function(){
+        for(let j=0;j<allPriorityColors.length;j++){
+            allPriorityColors[j].classList.remove("active");
+        }
+        priorityDivOneColor.classList.add("active");
+        modalPriorityColor=priorityDivOneColor.classList[0];
+
+     })
+      
+   }
+
+
 
 
 modalCont.addEventListener("keydown",function(e){
        let key=e.key;
         if(key=='Enter'){
         
-              createTicket(taskAreaCont.value);
+              createTicket(taskAreaCont.value,modalPriorityColor);
              taskAreaCont.value="";
              modalCont.style.display="none";
              addModal=!addModal;
@@ -30,9 +49,21 @@ modalCont.addEventListener("keydown",function(e){
        
 
 })
+ 
+   removeBtn.addEventListener("click",function(){
+    if(removeFlag){
+        removeBtn.style.color="black";
+
+    }else{
+        removeBtn.style.color="red";
+    }
+     removeFlag=!removeFlag;
+
+   })
 
 
-  function createTicket(task){
+
+  function createTicket(task,ticketColor){
 
 
     // <div class="ticket-cont">
@@ -43,7 +74,7 @@ modalCont.addEventListener("keydown",function(e){
 
       let ticketCont= document.createElement("div");
             ticketCont.setAttribute('class','ticket-cont');
-            ticketCont.innerHTML=`<div class="ticket-color green"></div>
+            ticketCont.innerHTML=`<div class="ticket-color ${ticketColor}"></div>
                                 <div class="ticket-id">#abd</div>
                                 <div class="task-area">${task}</div>`
             mainCont.appendChild(ticketCont);
