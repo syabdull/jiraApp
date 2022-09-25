fb8d8ef021a640ca84b13fa966ad1fb02d8041b2
syabdull <syab5050@gmail.com>
2022-09-25 08:59:55 +0530

    ticket functionality added

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 let addBtn=document.querySelector(".add-btn");
-let modal=document.querySelector(".modal-cont");
+let modalCont=document.querySelector(".modal-cont");
+let taskAreaCont=document.querySelector(".textarea-cont");
+let mainCont=document.querySelector(".main-cont");
 let addModal=true;
  
 
 addBtn.addEventListener("click",function(){
     if(addModal){
-        modal.style.display="flex";
+        modalCont.style.display="flex";
     }else{
-       modal.style.display="none";
+       modalCont.style.display="none";
     }
           addModal=!addModal;
 
 });
+
+
+modalCont.addEventListener("keydown",function(e){
+       let key=e.key;
+        if(key=='Enter'){
+        
+              createTicket(taskAreaCont.value);
+             taskAreaCont.value="";
+             modalCont.style.display="none";
+             addModal=!addModal;
+
+
+        }
+       
+
+})
+
+
+  function createTicket(task){
+
+
+    // <div class="ticket-cont">
+    //                   <div class="ticket-color green"></div>
+    //                   <div class="ticket-id">#abd</div>
+    //                   <div class="task-area">some task</div>
+    //                 </div>
+
+      let ticketCont= document.createElement("div");
+            ticketCont.setAttribute('class','ticket-cont');
+            ticketCont.innerHTML=`<div class="ticket-color green"></div>
+                                <div class="ticket-id">#abd</div>
+                                <div class="task-area">${task}</div>`
+            mainCont.appendChild(ticketCont);
+
+
+}
